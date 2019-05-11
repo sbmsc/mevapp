c0472771949757e0231153e4bf739829645890fa
sbmsc <chauhanshubham089@gmail.com>
2019-05-11 12:06:17 +0530

    fixing bugs

--- server/index.js
+++ server/index.js
@@ -22,6 +22,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
