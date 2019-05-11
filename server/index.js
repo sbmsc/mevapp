c52e8d08554ce328eb82efb57b0ecf54a0204ad0
sbmsc <chauhanshubham089@gmail.com>
2019-05-11 11:42:21 +0530

    ready to deploy

--- server/index.js
+++ server/index.js
@@ -11,6 +11,17 @@ app.use(cors());
 const posts = require("./routes/api/post");
 app.use("/api/posts", posts);
 
+//HANDLE PRODUCTION
+if (process.env.NODE_ENV === "production") {
+  //static folder
+  app.use(express.static(__dirname + "/public"));
+
+  //handle spa
+  app.get(/.*/, (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+  });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
