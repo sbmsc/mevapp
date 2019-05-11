f6c02f3c612d0dd18c5407f784cbba42aec7d0ae
sbmsc <chauhanshubham089@gmail.com>
2019-05-11 12:11:24 +0530

    fixing bugs

--- server/index.js
+++ server/index.js
@@ -14,14 +14,13 @@ app.use("/api/posts", posts);
 //HANDLE PRODUCTION
 if (process.env.NODE_ENV === "production") {
   //static folder
+  console.log("checkpoint");
   app.use(express.static(__dirname + "/public/"));
 
   //handle spa
-  app.get(/.*/, (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
-  });
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
 }
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
